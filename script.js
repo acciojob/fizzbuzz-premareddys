08b082f58281db50142b1905e1efc515a602e333
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-29 20:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let i=1;
+while(i<=20){
+	alert(i);
+	i++;
+}
+if(i%3==0){
+	console.log("Fizz"\n);
+}
+else if(i%5==0){
+	console.log("Buzz"\n);
+}
+else if(i%3==0 && i%5==0){
+	console.log("FizzBuzz"\n);
+}
+else{
+	console.log(i);
+}
